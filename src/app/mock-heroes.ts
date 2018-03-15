d7b10ccf5fce7edbc6acfe7c38e6c1547c7dfa8c
pa.gubin <pa.gubin@tensor.ru>
2018-03-15 20:56:47 +0300

    initial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'AngularJS'},
+  { id: 12, name: 'Backbone.js'},
+  { id: 13, name: 'Cappuccino'},
+  { id: 14, name: 'Chaplin.js'},
+  { id: 15, name: 'Echo'},
+  { id: 16, name: 'Ember.js'},
+  { id: 17, name: 'Enyo'},
+  { id: 18, name: 'Ext JS'},
+  { id: 19, name: 'Google Web Toolkit'},
+  { id: 20, name: 'JavaScriptMVC'}
+];
+
